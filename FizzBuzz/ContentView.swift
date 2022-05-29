338fc100f5fdf2004a28278df748105167917c5b
LuckDuracell <lukedru@yahoo.com>
2022-05-29 06:09:37 -0700

    Initial Commit

---
+++ FizzBuzz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FizzBuzz
+//
+//  Created by Luke Drushell on 5/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Luke Drushell on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
